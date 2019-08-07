f5f4732daceb57f34c7ed46fa8a66e8b180bd336
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-08-07 22:11:26 +0300

    Firebase 🔥 üzerinde oturuma göre işlemler
    Oturum açıksa farklı, kapalıysa farklı alanlara yönlendirme

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { withFirebase } from "../Firebase";
 
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
@@ -19,20 +20,48 @@ Bu alanda yapıalcak her değişiklik için alttaki alanlar değişmeli:
 - "../Navigation" 
 */
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation />
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-      <Route path={ROUTES.SOME_COMPONENT} component={SomeComponent} />
-    </div>
-  </Router>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+      authUser: null
+    };
+  }
+
+  componentDidMount() {
+    // Giriş yapıldı mı kontorlünü yapan dinleyici tanımalma
+    // !WARN: Listener component unmount olduğunda kaldırlmazsa performans kaybına neden olur.
+    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null });
+    });
+  }
+
+  componentWillUnmount() {
+    // Performans kaybını engellemek için listenerı kapatıyoruz
+    this.listener();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation authUser={this.state.authUser} />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+          <Route path={ROUTES.SOME_COMPONENT} component={SomeComponent} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+// Firebase'e erişimli App verisini döndürmemiz lazım, çünkü Firebase'i kullanıyor
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,35 +6,40 @@ import SignOutButton from "../SignOut";
 
 // !TIP: Sayfa yönlendirme işlemleri
 
-const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SOME_COMPONENT}>Some Component</Link>
-      </li>
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
-  </div>
+// AuthUser props'tan gelir.
+const Navigation = ({ authUser }) => (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+);
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SOME_COMPONENT}>Some Component</Link>
+    </li>
+  </ul>
 );
 
 export default Navigation;
