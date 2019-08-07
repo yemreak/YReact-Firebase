266563481569db34c3f1233df33aae7cfffb3596
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-08-07 19:36:22 +0300

    withFirebase için dökümantasyon eklendi ✨

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -3,6 +3,11 @@ import React from "react";
 const FirebaseContext = React.createContext(null);
 
 // !TIP: Firebase ile component kullanımı
+
+/**
+ * Firebase erişimi ile componenti çağırma
+ * @param {React.Component} Component Firebase erişimi verilecek React Componenti
+ */
 export const withFirebase = Component => props => (
   <FirebaseContext.Consumer>
     {firebase => <Component {...props} firebase={firebase} />}
