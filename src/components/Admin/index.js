544555ef0578b3b0c3bd49012cdcd14da91c11a9
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-08-15 15:59:07 +0300

    Admin 👨 sayfası eklendi

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const Admin = () => (
-  <div>
-    <h1>Admin</h1>
-  </div>
+import { withAuthorization } from "../Session";
+
+class AdminPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    // TIP: Her kullanıcı işlemi olduğunda çalışır
+    // WARN: users(), component unmount olduğunda kaldırlmazsa performans kaybına neden olur.
+    this.props.firebase.users().on("value", snapshot => {
+      const userObject = snapshot.val();
+
+      // Her user öğesini listeye kaydetme
+      const userList = Object.keys(userObject).map(key => ({
+        ...userObject[key],
+        uid: key
+      }));
+
+      this.setState({
+        users: userList,
+        loading: false
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.props.firebase.users().off();
+  }
+
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <div>
+        <h1> Admin </h1>
+        {loading && <div> Loading ... </div>}
+        <UserList users={users} />
+      </div>
+    );
+  }
+}
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map(user => (
+      <li key={user.uid}>
+        <span>
+          <strong>ID:</strong> {user.uid}
+        </span>
+        <span>
+          <strong>E-Mail:</strong> {user.email}
+        </span>
+        <span>
+          <strong>Username:</strong> {user.username}
+        </span>
+      </li>
+    ))}
+  </ul>
 );
 
-export default Admin;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AdminPage);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -25,6 +25,9 @@ const NavigationAuth = () => (
       <Link to={ROUTES.ACCOUNT}>Account</Link>
     </li>
     <li>
+      <Link to={ROUTES.ADMIN}>Admin</Link>
+    </li>
+    <li>
       <SignOutButton />
     </li>
   </ul>
